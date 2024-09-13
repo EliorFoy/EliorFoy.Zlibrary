9f5fec65536d9d955a9a0d9b563033242ea88f79
EliorFoy <1700118540@qq.com>
2024-09-13 19:53:51 +0800

    添加项目文件。

---
+++ EliorFoy.Zlibrary.GUI/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace EliorFoy.Zlibrary.GUI;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
